bfe83367ad076751041d8d2b415b36bbadd81cf4
danilo.vasquez <danilo.vasquez@bayteq.com>
2017-05-17 12:22:10 -0500

    add app

---
+++ app/src/main/java/com/dva/pollobar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dva.pollobar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
